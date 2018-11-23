314fec211ad7953d7f0602eab91bcef48351fdd1
Sandeep <sandeepg.97@gmail.com>
2018-11-23 10:47:16 +0530

    dymanic paths, db conn. - same thread check disabled

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db',connect_args = {'check_same_thread':False})
 Base.metadata.bind=engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
--- project.py
+++ project.py
@@ -1,10 +1,34 @@
 from flask import Flask
+
+from connection import session
+from database_setup import MenuItem, Restaurant
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    q = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = q.id) 
+    output = ''
+    for i in items:
+        output += i.name + '<br>'
+        output += i.price + '<br>'
+        output += i.description + '<br>'
+        output += '<br>'
+    return output
+
 @app.route('/hello')
 def hello_world():
-    return 'Bonjour Monsieur!'
+    q = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = q.id) 
+    output = ''
+    for i in items:
+        output += i.name + '<br>'
+        output += i.price + '<br>'
+        output += i.description + '<br>'
+        output += '<br>'
+    return output
 
 if __name__ == "__main__":
     app.debug = True
