8d98fba659db4b841bf8a51d9bab90998a245b91
Sandeep <sandeepg.97@gmail.com>
2018-11-24 12:14:04 +0530

    static - css file, add main page

--- project.py
+++ project.py
@@ -1,79 +1,72 @@
-from flask import Flask, redirect, render_template, request, url_for, flash 
-
+from flask import Flask, render_template, request, redirect, url_for, flash
 from connection import session
-from database_setup import MenuItem, Restaurant
+from database_setup import Restaurant, MenuItem
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/')
+def showRestaurants():
+    restaurants = session.query(Restaurant).all()
+    return render_template('showrestaurants.html',restaurants = restaurants)
+
+@app.route('/restaurants/<int:restaurant_id>/menu')
 def restaurantMenu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id) 
-    return render_template('menu.html',restaurant=restaurant,items=items)
-    
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    return render_template(
+        'menu.html', restaurant=restaurant, items=items)
 
-# Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/new/', methods = ['GET','POST'])
+@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
-        newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+        newItem = MenuItem(name=request.form['name'], description=request.form[
+                           'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
-        flash("Menu item added!")
-        return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
-    elif request.method == 'GET':
-        restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-        return render_template('newmenuitem.html',restaurant=restaurant)
+        flash("new menu item created!")
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
-        return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
+        restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+        return render_template('newmenuitem.html', restaurant = restaurant)
 
-# Task 2: Create route for editMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/', methods = ['GET','POST'])
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+           methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    if request.method == 'GET':
-        item = session.query(MenuItem).filter_by(restaurant_id = restaurant_id, id = menu_id).one()
-        return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id,item_name=item.name)
-    elif request.method == 'POST':
-        item = session.query(MenuItem).filter_by(restaurant_id = restaurant_id, id = menu_id).one()
-        item.name = request.form['name']
-        session.add(item)
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        if request.form['price']:
+            editedItem.price = request.form['price']    
+        session.add(editedItem)
         session.commit()
-        flash("Menu item updated!")
-        return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
+        flash("Menu Item has been edited")
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
-        return ''
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, item=editedItem)
 
-# Task 3: Create a route for deleteMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/', methods = ['GET','POST'])
+
+# DELETE MENU ITEM SOLUTION
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete',
+           methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    if request.method == 'GET':
-        restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-        item = session.query(MenuItem).filter_by(restaurant_id = restaurant_id, id = menu_id).one() 
-        return render_template('deletemenuitem.html',restaurant = restaurant, item = item) 
-    elif request.method == 'POST':
-        item = session.query(MenuItem).filter_by(restaurant_id = restaurant_id, id = menu_id).one() 
-        session.delete(item)
+    itemToDelete = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
         session.commit()
-        flash("Menu item deleted!")
-        return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))        
+        flash("Menu Item has been deleted")
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
-        pass 
+        restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+        return render_template('deletemenuitem.html', restaurant = restaurant ,item=itemToDelete)
 
-@app.route('/hello')
-def hello_world():
-    q = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(restaurant_id = q.id) 
-    output = ''
-    for i in items:
-        output += i.name + '<br>'
-        output += i.price + '<br>'
-        output += i.description + '<br>'
-        output += '<br>'
-    return output
 
-if __name__ == "__main__":
-    app.secret_key = 'super_secret_key '
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
